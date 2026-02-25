6c681ca721295e78bee211b20da661f019a2d41b
Daniele Karina dos Santos <danimistron@gmail.com>
2026-02-25 10:21:41 -0300

    config: cors

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'http://localhost:8080', 
+  timeout: 5000,
 });
 
 export default api;
